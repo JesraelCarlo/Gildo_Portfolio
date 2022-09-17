85133d88158eb0539c7aa0dcfd5f22fc54ed3523
JesraelCarlo <63578593+JesraelCarlo@users.noreply.github.com>
2022-09-17 22:32:01 +0800

    Add files via upload

---
+++ technoShop/index.php
@@ -0,0 +1,123 @@
+
+<? php inlcude 'cart.php' ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/style5.css">
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav>
+	<div class="wrapper">
+			
+		<div class="side">
+			<div class="logo">
+				<img src="image/logo2.png">
+			</div>
+			
+			<ul>
+				<li><a href="index.html">HOME</a></li>
+				<li><a href="index3.html">PRODUCT</a></li>
+				<li><a href="index5.html">CONCTACS</a></li>
+			</ul>
+
+			
+			
+		</div>
+
+			<div class="slideshow">
+
+				<div class="slideshow-item">
+					<div class="intro">
+						<img src="image/intro1.jpg">
+					<div class="slideshow-item-text">
+						<h5>GET WHAT YOU<br> DESERVE</h5>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					</div>
+					</div>
+				</div>
+
+				<div class="slideshow-item">
+					<div class="intro">
+						<img src="image/intro2.jpg">
+					<div class="slideshow-item-text">
+						<h5>GET WHAT YOU<br> DESERVE</h5>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					</div>
+					</div>
+				</div>
+
+				<div class="slideshow-item">
+					<div class="intro">
+						<img src="image/intro4.jpg">
+					<div class="slideshow-item-text">
+						<h5>GET WHAT YOU<br> DESERVE</h5>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+					</div>
+					</div>
+				</div>
+				
+
+			</div >
+
+
+			 <div class="content">
+
+            <a name="about"></a>
+
+           <section class="story">
+      
+
+                  <div class="container-fluid">
+
+                        <div class="section-data">
+
+                              <div class="row">
+                                    <div class="col-md-2"></div>
+                                    <div class="col-md-2 section-index wow fadeInUp" data-wow-delay="0.3s">01</div>
+                                    <div class="col-md-8 section-heading wow fadeInUp" data-wow-delay="0.4s">General presentation</div>
+                              </div>
+
+                              <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-6 section-subheading wow fadeInUp" data-wow-delay="0.5s"><h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus sapiente maxime a in consequatur ex tempora.</h1></div>
+                              </div>
+
+                              <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-6 section-info wow fadeInUp" data-wow-delay="0.6s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima nam quasi quod eaque incidunt aliquid provident accusantium nemo aperiam, voluptatibus nulla ex repellendus ad doloremque nisi iure, ipsam neque iste voluptate quidem sit sed fugiat! Consequatur, aut vel. Voluptate?</div>
+                              </div>
+
+                               <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-8 more wow fadeInUp" data-wow-delay="0.7s">
+                                          <button id="btn">Know more &#8594;</button>
+                                    </div>
+                              </div>
+
+                        </div>
+
+                  </div>
+
+           </section>
+
+		</div>
+	</div>
+</nav>
+
+	
+		
+
+		
+
+				
+
+				
+			
+</body>
+</html>
